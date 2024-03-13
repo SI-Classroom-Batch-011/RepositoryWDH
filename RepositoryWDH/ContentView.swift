8c2ebfb732e3e0f99bff72dfefad0cc08d6c09e5
joshur19 <rueck.joshua@gmail.com>
2024-03-13 16:46:36 +0100

    Initial Commit

---
+++ RepositoryWDH/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RepositoryWDH
+//
+//  Created by Joshua Rück on 13.03.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RepositoryWDH/RepositoryWDHApp.swift
@@ -0,0 +1,17 @@
+//
+//  RepositoryWDHApp.swift
+//  RepositoryWDH
+//
+//  Created by Joshua Rück on 13.03.24.
+//
+
+import SwiftUI
+
+@main
+struct RepositoryWDHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
